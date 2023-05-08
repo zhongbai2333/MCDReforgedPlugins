2c421e89e327cc3cdf55c87aca47ccdf0e801340
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2023-05-08 19:13:19 +0800

    Delete dict_command_registration directory

--- dict_command_registration/dict_command_registration/exceptions.py
+++
@@ -1,8 +0,0 @@
-__all__ = [
-    "MissingRequiredAttribute",
-]
-
-
-class MissingRequiredAttribute(Exception):
-    def __init__(self, name: str):
-        super().__init__(f"Missing required attribute \"{name}\"")
